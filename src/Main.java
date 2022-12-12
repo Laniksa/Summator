46e279ed390af049d190c64e3ac2dcee6d75d58d
Регина <rmashkovtseva@gmail.com>
2022-12-12 22:55:10 +0300

    Программа полностью переделана, с целью ввода неограниченного количества слагаемых.

--- src/Main.java
+++ src/Main.java
@@ -1,62 +1,36 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 
 class Main {
 
-        public static double sum ( double num1, double num2, double num3){
-            return num1 + num2 + num3;
-        }
-        public static void main (String[]args){
-            double num1;
-            double num2;
-            double num3;
+    public static int count = 1;
+
+    public static void main(String[] args) {
+        Scanner slog = new Scanner(System.in);
+        System.out.println("Введите количество слагаемых ");
+        int kol = slog.nextInt();
+        List<Double> list = new ArrayList<>();
+
+        for (int i = 0; i < kol; i++) {
 
             Scanner scan = new Scanner(System.in);
-
             while (true) {
-                System.out.print("Введите первое число: ");
-                String line1 = scan.nextLine();
+                double num;
+                System.out.print("Введите " + count + "-e число: ");
+                String line = scan.nextLine();
                 try {
-                    num1 = Double.parseDouble(line1);
+                    num = Double.parseDouble(line);
+                    count++;
+                    list.add(num);
+                    System.out.println(list);//здесь нужно выводить сумму чисел массива, для этого нужен отдельный метод
                     break;
-
                 } catch (NumberFormatException e) {
-                    System.out.println("Вы ввели не верное 1-е число ");
+                    System.out.println("Вы ввели не верное " + count + "-e число ");
                 }
             }
 
-
-
-            while (true) {
-                System.out.print("Введите второе число: ");
-                String line2 = scan.nextLine();
-
-                try {
-                    num2 = Double.parseDouble(line2);
-                    break;
-
-                } catch (NumberFormatException e) {
-                    System.out.println("Вы ввели не верное 2-е число ");
-                }
-            }
-
-
-            while (true) {
-                System.out.print("Введите третье число: ");
-                String line3 = scan.nextLine();
-
-                try {
-                    num3 = Double.parseDouble(line3);
-                    break;
-
-                } catch (NumberFormatException e) {
-                    System.out.println("Вы ввели не верное 3-е число ");
-                }
-            }
-
-
-            double result = sum(num1, num2, num3);
-
-            System.out.printf("Сумма чисел равна: " + "%.1f", result);// "%.1f"- позволяет обрезать цифры после занятой до 1 (1f)
         }
     }
+}
